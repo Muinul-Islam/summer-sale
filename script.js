39c9f4aec2489a5d8ac35b1ad68f1515f534c834
Muinul-Islam <muinul1022@gmail.com>
2023-08-20 02:20:27 +0600

    script added

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
-function addToCart(index){
-  const cardTitle = document.getElementsByClassName('card-title')[index].innerText;
+let totalPrice = 0;
+let discount = 0;
+function addToCart(target) {
+  const cardTitle =
+    document.getElementsByClassName("card-title")[target].innerText;
 
-  const priceString = document.getElementsByClassName('price')[index].innerText;
+  const priceString = document.getElementsByClassName("price")[target].innerText;
 
   const price = parseFloat(priceString);
-  console.log(cardTitle , price);
+
+  const li = document.createElement("li");
+  li.innerText = cardTitle;
+
+  const listItem = document.getElementById("list-item");
+  listItem.appendChild(li);
+
+  totalPrice = parseFloat(totalPrice) + price;
+  const totalPriceAmount = document.getElementById("total-price").innerText = totalPrice.toFixed(2);
+
+  const btnSell = document.getElementById('btn-sell');
+  
 }
