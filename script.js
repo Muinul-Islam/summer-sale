fd917ffa91769659325e34bf0928eb0e35dddb4b
Muinul-Islam <muinul1022@gmail.com>
2023-08-20 13:07:26 +0600

    script added

--- script.js
+++ script.js
@@ -1,22 +1,53 @@
 let totalPrice = 0;
+let total = 0;
 let discount = 0;
+
 function addToCart(target) {
   const cardTitle =
     document.getElementsByClassName("card-title")[target].innerText;
 
-  const priceString = document.getElementsByClassName("price")[target].innerText;
+  const priceString =
+    document.getElementsByClassName("price")[target].innerText;
 
   const price = parseFloat(priceString);
 
-  const li = document.createElement("li");
-  li.innerText = cardTitle;
-
   const listItem = document.getElementById("list-item");
-  listItem.appendChild(li);
+  const count = listItem.childElementCount;
 
-  totalPrice = parseFloat(totalPrice) + price;
-  const totalPriceAmount = document.getElementById("total-price").innerText = totalPrice.toFixed(2);
+  const p = document.createElement("p");
+  p.innerHTML = `${count + 1}. ${cardTitle}`;
 
-  const btnSell = document.getElementById('btn-sell');
-  
+  listItem.appendChild(p);
+
+  totalPrice += price;
+  const totalPriceAmount = document.getElementById("total-price");
+  totalPriceAmount.innerText = totalPrice.toFixed(2);
+  document.getElementById('total').innerText = totalPrice.toFixed(2);
+
+ 
+  if (totalPrice === 0) {
+    document.getElementById('make-purchase').setAttribute('disabled' , 'true');
+  }
+  else{
+    document.getElementById('make-purchase').removeAttribute('disabled');
+  }
 }
+
+document.getElementById("coupon-apply").addEventListener("click", function () {
+  const couponCode = document.getElementById("coupon-code").value;
+
+  if(couponCode === ' '){
+    document.getElementById('coupon-apply').setAttribute('disabled' , 'true');
+  }
+  else if (couponCode === "SELL200") {
+    discount = totalPrice * 0.2;
+
+    const discountOnCoupon = document.getElementById('discount');
+    discountOnCoupon.innerText = discount.toFixed(2);
+
+    total = totalPrice - discount;
+    document.getElementById('total').innerText = total.toFixed(2);
+   
+  }
+  document.getElementById('coupon-code').value = '';
+});
