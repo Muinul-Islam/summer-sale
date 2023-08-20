14fe8c659264a9cf94f083597993a1659c3cc195
Muinul-Islam <muinul1022@gmail.com>
2023-08-20 17:06:43 +0600

    Almost done

--- script.js
+++ script.js
@@ -24,34 +24,36 @@ function addToCart(target) {
   totalPriceAmount.innerText = totalPrice.toFixed(2);
   document.getElementById("total").innerText = totalPrice.toFixed(2);
 
-  if (totalPrice <= 0) {
-    document.getElementById("make-purchase").setAttribute("disabled", "true");
-  } else {
-    document.getElementById("make-purchase").removeAttribute("disabled");
-  }
+  document.getElementById("coupon-apply").addEventListener("click", function () {
+    const couponCode = document.getElementById("coupon-code").value;
+
+    if (totalPrice >= 200) {
+      if (couponCode === "SELL200") {
+        discount = totalPrice * 0.2;
+  
+        const discountOnCoupon = document.getElementById("discount");
+        discountOnCoupon.innerText = discount.toFixed(2);
+  
+        total = totalPrice - discount;
+        document.getElementById("total").innerText = total.toFixed(2);
+      }
+    }
+  
+    document.getElementById("coupon-code").value = "";
+  });
 }
 
-document.getElementById("coupon-apply").addEventListener("click", function () {
-  const couponCode = document.getElementById("coupon-code").value;
 
-  if (couponCode === " ") {
-    document.getElementById("coupon-apply").setAttribute("disabled", "true");
-  }
-  if (totalPrice >= 200) {
-    if (couponCode === "SELL200") {
-      discount = totalPrice * 0.2;
-
-      const discountOnCoupon = document.getElementById("discount");
-      discountOnCoupon.innerText = discount.toFixed(2);
-
-      total = totalPrice - discount;
-      document.getElementById("total").innerText = total.toFixed(2);
-    }
-  }
-
-  document.getElementById("coupon-code").value = "";
-});
 
 document.getElementById("btn-home").addEventListener("click", function () {
-  window.location.href = 'index.html';
+  totalPrice = 0;
+  total = 0;
+  discount = 0;
+
+  document.getElementById('total-price').innerText = '00';
+  document.getElementById('discount').innerText = '00';
+  document.getElementById('total').innerText = '00';
+
+document.getElementById('list-item').innerHTML = '';
+
 });
