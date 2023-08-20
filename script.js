0a06149b57c56a8c1a744e3df4c1c63da9377d77
Muinul-Islam <muinul1022@gmail.com>
2023-08-20 18:53:31 +0600

    modal added

--- script.js
+++ script.js
@@ -3,11 +3,9 @@ let total = 0;
 let discount = 0;
 
 function addToCart(target) {
-  const cardTitle =
-    document.getElementsByClassName("card-title")[target].innerText;
+  const cardTitle = document.getElementsByClassName("card-title")[target].innerText;
 
-  const priceString =
-    document.getElementsByClassName("price")[target].innerText;
+  const priceString = document.getElementsByClassName("price")[target].innerText;
 
   const price = parseFloat(priceString);
 
@@ -27,18 +25,19 @@ function addToCart(target) {
   document.getElementById("coupon-apply").addEventListener("click", function () {
     const couponCode = document.getElementById("coupon-code").value;
 
+
     if (totalPrice >= 200) {
       if (couponCode === "SELL200") {
         discount = totalPrice * 0.2;
-  
+
         const discountOnCoupon = document.getElementById("discount");
         discountOnCoupon.innerText = discount.toFixed(2);
-  
+
         total = totalPrice - discount;
         document.getElementById("total").innerText = total.toFixed(2);
       }
     }
-  
+
     document.getElementById("coupon-code").value = "";
   });
 }
@@ -54,6 +53,6 @@ document.getElementById("btn-home").addEventListener("click", function () {
   document.getElementById('discount').innerText = '00';
   document.getElementById('total').innerText = '00';
 
-document.getElementById('list-item').innerHTML = '';
+  document.getElementById('list-item').innerHTML = '';
 
 });
